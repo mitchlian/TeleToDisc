cc40044572cb0f9a1219f44469cc257dd29c5fe3
Midchmemes <angelbeattm@gmail.com>
2025-09-22 21:31:44 +0800

    new addition
    Added post requests to also forward attachments and links
    Also changed some logging to info instead of all errors
    and some other stuff which i forgot

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class clientException(Exception):
 FILE_PATH = 'log.txt'
 logger = logging.getLogger()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler = TimedRotatingFileHandler("bot.log",when='midnight',endcoding='utf-8')
+handler = TimedRotatingFileHandler("bot.log",when='midnight',encoding='utf-8')
 
 logger.setLevel(logging.INFO)
 handler.setFormatter(formatter)
@@ -31,24 +31,46 @@ Intents = discord.Intents.default() # Allows basic perms
 Intents.message_content = True # enables on_message events (so your bot sees text messages).
 client = discord.Client(intents = Intents) #creates the bot client, telling Discord “only send me the events I asked for.”
 
+def forward_attachments(attachment):
+    if attachment.content_type.startswith("image/"):
+        #Call telegram sendPhoto
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendPhoto"
+        params = {"chat_id": TELEGRAM_CHAT_ID, "photo": attachment.url}
+        try:
+            requests.post(url,params=params)
+        except Exception as e:
+            logger.error(f"sendPhoto failed. \n {e}")
+    else:
+        #Call telegram sendDocument
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendDocument"
+        params = {"chat_id": TELEGRAM_CHAT_ID, "document": attachment.url}
+        try:
+            requests.post(url,params=params)
+        except Exception as e:
+            logger.error(f"sendDocument failed. \n {e}")
+
 @client.event
 async def on_ready():
-    logger.error(f"We have logged in as {client.user}")
-
+    logger.info(f"We have logged in as {client.user}")
 
 @client.event
 async def on_message(message):
     # ignore message from ourselves
     if message.author == client.user:
         return 
-    
-    text = f"[{message.guild.name}] #{message.channel.name} | {message.author.name}: {message.content}"; # Craft the message
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}" # Pass telegram url into requests.get
-    params = {"chat_id": TELEGRAM_CHAT_ID, "text": text}
-    try:
-        requests.get(url,params=params)
-    except Exception as e:
-        logger.error(f"Request.get failed. \n {e}")
+    if message.content:
+        text = f"[{message.guild.name}] #{message.channel.name} | {message.author.name}: {message.content}"; # Craft the message
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}" # Pass telegram url into requests.get
+        params = {"chat_id": TELEGRAM_CHAT_ID, "text": text}
+        try:
+            requests.post(url,params=params)
+        except Exception as e:
+            logger.error(f"sendMessage failed. \n {e}")
+
+    for attachment in message.attachments:
+        forward_attachments(attachment)
+
+
 
 try:
     client.run(DISCORD_TOKEN)
